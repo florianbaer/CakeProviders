80fb03380bbb0d1b140156165ad117e4515cdede
binaryfr3ak <binaryfr3ak@gmail.com>
2018-04-24 20:32:25 +0200

    add old server source

--- server/index.js
+++ server/index.js
@@ -24,72 +24,89 @@ async function start() {
     await builder.build()
   }
 
-  app.get('/api', function (req, res, next) {
-    axios.get('https://api-v2.swissunihockey.ch/api/games/', {
-      params: {
-        mode: 'team',
-        team_id: 429603,
-        season: 2017,
-        games_per_page: 30
-      }
-    }).then(ans => {
-      console.log(ans)
-      if (!error && ans.statusCode === 200) {
-        var responseBody = {
-          games: []
-        }
+  app.get('/api', function(req, res, next) {
 
-        var players;
-        try {
-          var fileread = fs.readFileSync('./team.json', 'utf-8');
-          console.log('read successful');
-          players = JSON.parse(fileread);
-        }
-        catch (err) {
-          console.log('read not successful')
-          if (err.code === 'ENOENT') {
-            console.log('File not found!');
-          } else {
-            throw err;
-          }
-        }
-        console.log(' output of file is coming')
-        console.log(JSON.stringify(players));
-        for (var game of JSON.parse(body).data.regions[0].rows) {
+    request({
+        uri: 'https://api-v2.swissunihockey.ch/api/games/',
+        qs: {
+          mode: 'team',
+          team_id: 429603,
+          season: 2017,
+          games_per_page: 35
+        }},
 
-          var localGame = {
-            game: null,
-            Players: new Array()
+      function(error, response, body) {
+
+        if (!error && response.statusCode === 200) {
+          var responseBody = {
+            games: []
           }
 
+          var players = JSON.parse("{\n" +
+            "  \"players\": [\n" +
+            "    \"Abt Simon\",\n" +
+            "    \"Bär Florian\",\n" +
+            "    \"Crivelli Giona\",\n" +
+            "    \"Fiechter Steven\",\n" +
+            "    \"Flütsch Peter\",\n" +
+            "    \"Furger Adrian\",\n" +
+            "    \"Grüter Markus\",\n" +
+            "    \"Hietanen Aki\",\n" +
+            "    \"Koutny Jiri\",\n" +
+            "    \"Laely Marco\",\n" +
+            "    \"Menon Andrea\",\n" +
+            "    \"Grüter Thomas\",\n" +
+            "    \"Müller Matthias\",\n" +
+            "    \"Schelbert Joshua\",\n" +
+            "    \"Nilsson Billy\",\n" +
+            "    \"Nilsson Petter\",\n" +
+            "    \"Poletti Sandro\",\n" +
+            "    \"Schelbert Yannick\",\n" +
+            "    \"Staub Manuel\",\n" +
+            "    \"Suter Tassio\",\n" +
+            "    \"Thunvall Marcus\",\n" +
+            "    \"Uhr Adrian\"\n" +
+            "  ]\n" +
+            "}\n");//fs.readFileSync('./team.json', 'utf8'));
 
-          if (game.cells[2].text[0] == 'Zug United' && (game.cells[0].text[0] == 'heute' || moment(game.cells[0].text[0], 'DD.MM.YYYY') >= moment('26.09.2017', 'DD.MM.YYYY'))) {
-            localGame.game = {
-              date: game.cells[0].text[0],
-              opponent: game.cells[3].text[0],
-              location: game.cells[1].text[0]
+          console.log(JSON.stringify(players));
+          for (var game of JSON.parse(body).data.regions[0].rows) {
+
+            var localGame = {
+              game: null,
+              Players: new Array()
             }
 
-            for (var i = 0; i < 3; i++) {
-              var player = players.players.pop();
-              console.log(JSON.stringify(player));
-              players.players.unshift(player);
-              localGame.Players.push(player);
+
+
+            if (game.cells[2].text[0] == 'Zug United' && ( game.cells[0].text[0] == 'heute' || moment(game.cells[0].text[0], 'DD.MM.YYYY') >= moment('26.09.2017', 'DD.MM.YYYY'))) {
+              localGame.game = {
+                date: game.cells[0].text[0],
+                opponent: game.cells[3].text[0],
+                location: game.cells[1].text[0]
+              }
+
+              for (var i = 0; i < 3; i++) {
+                var player = players.players.pop();
+                console.log(JSON.stringify(player));
+                players.players.unshift(player);
+                localGame.Players.push(player);
+              }
+
+
+              responseBody.games.push(localGame);
+
             }
-
-
-            responseBody.games.push(localGame);
-
           }
+
+
+
+          res.json(responseBody.games);
+        } else {
+          res.json(error);
         }
 
-
-        res.json(responseBody.games);
-      }
-      else {
-        res.json(error);
-      }
-    });
+      });
   });
 
 // Give nuxt middleware to express
